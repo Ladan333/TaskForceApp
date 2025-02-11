9f3019a8933944e4b472c9a551210002666d5d66
Rasmus Kjell <rasmus.kjell@hotmail.com>
2025-02-11 10:03:59 +0100

    Add files via upload

---
+++ TaskForceApp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .basic-grid{
+            text-align: center;
+            border: solid 1px black;
+        }
+    </style>
+</head>
+<body>
+    <div class="basic-grid">
+        <form action="TaskForceApp.php" method="POST">
+            <label for="newTask">Add a task</label><br>
+            <input type="text" id="newTask" name="newTask"></input>
+            <input type="submit" value="Submit"></input>
+        </form>
+    </div>
+    <div>
+        <?php
+            echo $_POST["newTask"];
+        ?>
+    </div>
+</body>
+</html>
